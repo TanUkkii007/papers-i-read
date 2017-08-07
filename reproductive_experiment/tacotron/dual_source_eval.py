84a704cb35956a382f548be651d1a99e105838ff
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-08-08 04:10:46 +0900

    fix dual_source_eval.py

--- reproductive_experiment/tacotron/dual_source_eval.py
+++ reproductive_experiment/tacotron/dual_source_eval.py
@@ -34,6 +34,11 @@ def eval():
     # ToDo: use load_dual_source_vocabrary
     phone2idx, idx2phone = load_phone_ja()
 
+    ah1 = g.attention_final_state.state1_alignment_history
+    alignment_history1 = ah1.gather(tf.range(0, ah.size()))
+    ah2 = g.attention_final_state.state2_alignment_history
+    alignment_history2 = ah1.gather(tf.range(0, ah.size()))
+
     with g.graph.as_default():
         sv = tf.train.Supervisor()
         with sv.managed_session(config=tf.ConfigProto(
@@ -57,18 +62,20 @@ def eval():
                 outputs1[:, j, :] = _outputs1[:, j, :]
             outputs2 = sess.run(g.outputs2, {g.outputs1: outputs1})
 
-            attention_final_state = sess.run(
-                g.attention_final_state, {g.x1: X1,
-                                          g.x2: X2,
-                                          g.y: outputs1})
+            alignment_history1 = sess.run(alignment_history1,
+                                          {g.x1: X1,
+                                           g.x2: X2,
+                                           g.y: outputs1})
+            alignment_history2 = sess.run(alignment_history2,
+                                          {g.x1: X1,
+                                           g.x2: X2,
+                                           g.y: outputs1})
 
     # Generate wav files
     if not os.path.exists(hp.outputdir): os.mkdir(hp.outputdir)
     with codecs.open(hp.outputdir + '/text.txt', 'w', 'utf-8') as fout:
         for i, (x1, x2, s, a1, a2) in enumerate(
-                zip(X1, X2, outputs2,
-                    attention_final_state.state1_alignment_history,
-                    attention_final_state.state2_alignment_history)):
+                zip(X1, X2, outputs2, alignment_history1, alignment_history2)):
             # write text
             fout.write(
                 str(i) + "\t" + "".join(idx2char_kana[idx]
@@ -85,13 +92,13 @@ def eval():
                 audio = spectrogram2wav(s**hp.power)
             write(hp.outputdir + "/{}_{}.wav".format(mname, i), hp.sr, audio)
 
-            visualize_attention(a1[i], [
+            visualize_attention(a1, [
                 idx2char_kana[idx] for idx in np.fromstring(x1, np.int32)
             ])
             save_figure(hp.outputdir + "/{}_{}_attention1.png".format(
                 mname, i))
             visualize_attention(
-                a2[i], [idx2phone[idx] for idx in np.fromstring(x2, np.int32)])
+                a2, [idx2phone[idx] for idx in np.fromstring(x2, np.int32)])
             save_figure(hp.outputdir + "/{}_{}_attention2.png".format(
                 mname, i))
 
